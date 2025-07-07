dda5b0ca9cf8dfdfb6416475bfe47dbf71b7798c
Poovarasan0909 <poovarasanm0909@gmail.com>
2025-07-07 22:47:24 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 "use client"
-//import { useRouter } from "next/navigation"
-import Link from 'next/link';
-
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-    //const router = useRouter();
+    const router = useRouter();
 
-    //const handleOnClick = () => {
-    //    router.push("/resume")
-    //}
+    const handleOnClick = () => {
+        router.push("/resume")
+    }
    
   return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-red-950">
-          <h1>tesr</h1>
-          <Link href="/resume">
-              <button> View Resume1 </button>
-</Link>
-
-          {/*<button onClick={() => router.push("/test")}> View Test Page </button>*/}
+      <h1>tesr</h1>
+          <button onClick={() => handleOnClick()}> View Resume </button>
+          <button onClick={() => router.push("/test")}> View Test Page </button>
     </div>
   );
 }
