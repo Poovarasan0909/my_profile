22f1e8cd355362a01d922771be9a351636fae225
Poovarasan0909 <poovarasanm0909@gmail.com>
2025-08-25 09:58:43 +0530

    build bug

--- src/components/AnimationSection.tsx
+++ src/components/AnimationSection.tsx
@@ -1,13 +1,13 @@
 
 import { motion } from "framer-motion";
 
-const AnimatedSection = ({ children }) => {
+const AnimatedSection = ({ children }:any) => {
   return (
     <motion.section
-    //   initial={{ opacity: 0, y: 75 }}
-    //   whileInView={{ opacity: 1, y: 0 }}
-    //   transition={{ duration: 0.5, delay: 0.1 }}
-    //   viewport={{ once: true }} 
+      initial={{ opacity: 0, y: 75 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
+      viewport={{ once: true }} 
     >
       {children}
     </motion.section>
